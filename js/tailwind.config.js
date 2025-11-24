002575797535a76a936cec5de1d9ae4b21b8dc8d
KrunalSavaj <developer.krunalsavaj@gmail.com>
2025-11-24 08:44:59 +0530

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        'sans': ['poppins', ' sans-serif',],
+        'serif': ['Roboto', 'sans-serif',],
+      },
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
+    }
+  }
+ 
